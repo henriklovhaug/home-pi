a5a15062e5ce800f1baf49241e6bbd14b16ec623
Henrik Løvhaug <henrik.lovhaug@gmail.com>
2023-09-25 21:52:22 +0200

    Fix build?

--- src-tauri/src/bus/bus_routes.rs
+++ src-tauri/src/bus/bus_routes.rs
@@ -1,1 +1,45 @@
+use std::sync::OnceLock;
 
+pub static BUS: OnceLock<Bus> = OnceLock::new();
+
+use std::collections::HashMap;
+
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct Bus {
+    pub hour: HashMap<u8, Vec<u8>>,
+}
+
+impl Bus {
+    pub fn init() -> Bus {
+        let mut m = HashMap::new();
+        m.insert(5, vec![49]);
+        m.insert(6, vec![9, 29, 46]);
+        m.insert(7, vec![1, 11, 21, 31, 43, 53]);
+        m.insert(8, vec![3, 13, 23, 33, 44, 55]);
+        m.insert(9, vec![4, 13, 23, 33, 48, 58]);
+        m.insert(10, vec![11, 23, 33, 42, 52]);
+        m.insert(11, vec![2, 12, 22, 32, 42, 52]);
+        m.insert(12, vec![2, 12, 22, 32, 42, 57]);
+        m.insert(13, vec![12, 22, 32, 42, 52]);
+        m.insert(14, vec![2, 12, 22, 32, 48, 58]);
+        m.insert(15, vec![8, 17, 27, 37, 47, 57]);
+        m.insert(16, vec![7, 17, 27, 37, 53]);
+        m.insert(17, vec![3, 13, 23, 33, 41, 51]);
+        m.insert(18, vec![1, 16, 26, 36, 46]);
+        m.insert(19, vec![1, 11, 26, 36, 46, 56]);
+        m.insert(20, vec![16, 26]);
+        Bus { hour: m }
+    }
+
+    pub fn get(&self, hour: u8) -> (u8, Vec<u8>) {
+        if hour < 5 {
+            return (5, self.hour.get(&5).unwrap().clone());
+        } else if hour > 20 {
+            return (20, self.hour.get(&20).unwrap().clone());
+        } else {
+            return (hour, self.hour.get(&hour).unwrap().clone());
+        }
+    }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use bus::bus_routes::{Bus, BUS};
 
 mod bus;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![say_hello])
+        .invoke_handler(tauri::generate_handler![say_hello, get_bus])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -14,4 +15,10 @@ fn main() {
 #[tauri::command]
 fn say_hello() -> Result<String, String> {
     Ok("Hello from Rust!".to_string())
+}
+
+#[tauri::command]
+fn get_bus(hour: u8) -> Result<(u8, Vec<u8>), String> {
+    let bus = BUS.get_or_init(|| Bus::init());
+    Ok(bus.get(hour))
 }
