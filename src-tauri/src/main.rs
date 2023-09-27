ef55d4211dee9f3b9e7fcfe51162c9cb103251b7
Henrik Løvhaug <henrik.lovhaug@gmail.com>
2023-09-27 12:58:57 +0200

    Clippy

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -27,13 +27,13 @@ fn say_hello() -> Result<String, String> {
 
 #[tauri::command]
 fn get_bus(hour: u8) -> Result<(u8, Vec<u8>), String> {
-    let bus = BUS.get_or_init(|| Bus::init());
+    let bus = BUS.get_or_init(Bus::init);
     Ok(bus.get(hour))
 }
 
 #[tauri::command]
 fn get_bus_now() -> Result<(u8, Vec<u8>), String> {
-    let bus = BUS.get_or_init(|| Bus::init());
+    let bus = BUS.get_or_init(Bus::init);
     let utc: DateTime<Tz> = Utc::now().with_timezone(&Paris);
     let hour = utc.hour();
     Ok(bus.get(
@@ -44,7 +44,7 @@ fn get_bus_now() -> Result<(u8, Vec<u8>), String> {
 
 #[tauri::command]
 fn get_next_n_bus(n: u32) -> Vec<BusTime> {
-    let bus = BUS.get_or_init(|| Bus::init());
+    let bus = BUS.get_or_init(Bus::init);
     let utc: DateTime<Tz> = Utc::now().with_timezone(&Paris);
     let hour = utc.hour() as u8;
     let minute = utc.minute() as u8;
